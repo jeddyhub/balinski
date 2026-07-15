ae3cc43eb6f5775a742c60f5a933f290651c705f
Ivan Martinez Comas <imcnavi20@gmail.com>
2026-07-16 00:01:26 +0200

    first part of balinski proved modulo "the vector span of a set is the same as the vector span of its convex hull".

--- Polyhedral/Icarm/lattice_graph.lean
+++ Polyhedral/Icarm/lattice_graph.lean
@@ -3,44 +3,71 @@ import Polyhedral.Mathlib.Geometry.Convex.ConvexSpace.Polytope.Lattice
 import Mathlib.Combinatorics.SimpleGraph.Basic
 import Mathlib.Analysis.Convex.Extreme
 import Mathlib.Analysis.Convex.Segment
+import Mathlib.Analysis.Convex.Hull
 import Polyhedral.Polyhedral.Basic
 import Polyhedral.Polyhedral.Faces
 import Polyhedral.Mathlib.Geometry.Convex.ConvexSpace.Polytope.Face
+import Mathlib.LinearAlgebra.AffineSpace.FiniteDimensional
 
 open Convexity Convex SimpleGraph
 
-variable (R : Type*)
 variable {V A : Type*}
 
 open Convexity ConvexSet Affine
 
-variable [LinearOrder R] [Ring R] [IsStrictOrderedRing R]
-variable [AddCommGroup V] [Module R V] [AddTorsor V A]
-attribute [local instance] AddTorsor.toConvexSpace
+variable [AddCommGroup V] [Module ℝ V] [AddTorsor V A]
 
 noncomputable def dimPolytope (C : Set A) : ℕ :=
-  Module.finrank R (affineSpan R C).direction
-
+  Module.finrank ℝ (vectorSpan ℝ C) --(affineSpan ℝ C).direction
 section
 
 variable
-  (R : Type*)
   (X : Type*)
-  [Ring R]
-  [PartialOrder R]
-  [IsStrictOrderedRing R]
-  [ConvexSpace R X]
+  [ConvexSpace ℝ X]
   [AddCommGroup X]
-  [Module R X]
+  [Module ℝ X]
 
 structure isGraphPolytopeGenerated
-    (P : Polytope R X) (V : Finset X) (G : SimpleGraph V) : Prop where
-  h_carrier : Convexity.convexHull R V = P.carrier
+    (P : Polytope ℝ X) (V : Finset X) (G : SimpleGraph V) : Prop where
+  h_carrier : _root_.convexHull ℝ V = P.carrier
   h_2 : ∀ (i j : X),  (hi : i ∈ V) → (hj : j ∈ V) →
     G.Adj ⟨i,hi⟩ ⟨j,hj⟩ ↔
-    Convexity.IsExtreme R P.carrier [i -[R] j]
+    Convexity.IsExtreme ℝ P.carrier [i -[ℝ] j]
 
-theorem balinski {P : Polytope R X} {V : Finset X} {G : SimpleGraph V}
-(hG : isGraphPolytopeGenerated R X P V G) :
-  IsVertexConnected G (dimPolytope R (V := X) (P.carrier))
-    := sorry --the graph G with isGraphPolytopeGenerated P V G is d-connected for every d-polytope P
+lemma vectorSpan_of_convexHull (S : Set X) : vectorSpan ℝ (_root_.convexHull ℝ S) = (vectorSpan ℝ S) := by
+  ext x ; unfold vectorSpan
+  simp only [Submodule.mem_span]
+  constructor <;> intro h1 p h2
+  · specialize h1 p
+    apply h1
+    sorry
+  · specialize h1 p
+    apply h1
+    sorry
+
+theorem balinski {P : Polytope ℝ X} {V : Finset X} {G : SimpleGraph V} (hV : Finset.Nonempty V)
+(hG : isGraphPolytopeGenerated X P V G) :
+  IsVertexConnected G (dimPolytope (V := X) (P.carrier))
+    := by
+        classical
+        unfold IsVertexConnected
+        constructor
+        · unfold dimPolytope
+          set n := Nat.card V
+          rw[← hG.h_carrier, vectorSpan_of_convexHull]
+          have : ↑V = Finset.image id V := by simp
+          rw[this]
+          have hn0 :  1 ≤ n := by simp[n] ; exact hV
+          have := finrank_vectorSpan_image_finset_le ℝ id V (n := n - 1) (by unfold n at *; simp at this ⊢;grind)
+          grind
+        · intros S H
+          simp only [connected_iff_exists_forall_reachable]
+          have v₁ : @Set.Elem (↥V) (↑S)ᶜ := sorry
+          apply Exists.intro v₁
+          · intro w
+            unfold Reachable
+            constructor
+            constructor
+            · sorry
+            · sorry
+            · sorry
